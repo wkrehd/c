b3e76cae797d2ecb319ce2b0792432e6190e33d8
vhswo <cba0898@gmail.com>
2023-04-04 15:51:16 +0900

    2023/4/4
    escape문자
    서식문자(제어문자)
    
    c언어 수업자료 백업

---
+++ hellow world만들기/hellow world만들기/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	printf("%s %s\n", "Hello,",  "World");
+	printf("375 X 543 = %d\n", 375 * 543);
+	printf("%.4f\n", 3.14159264);
+	printf("%c %s\n", 'C', "Language");
+
+	/*printf("%d + %d = %d\n", 5, 10, 5+10);
+	printf("%d + %d = \n", 5, 10, 5 + 10);
+	printf("%d + %d = %d\n\n", 5, 10);
+
+	printf("실수형 상수 : %f\n", 10.5);
+	printf("정수형 상수 : %d\n", 10.5);
+	printf("실수형 상수 : %f\n\n", 10);
+	printf("실수형 상수 : %.2f\n\n", 10.5);
+	printf("문자 %c 의 ASCII코드 값은 %d 인 문자는 %d 이다.\n", 'a', 'a');
+	printf("ASCII코드 값이 %d 인 문자는 %c dlek.\n", 66, 66);
+	printf("%c = %d(?)\n\n", '0', '0');
+	printf("%d + %d = %d\n", 1, 1, 1 + 1);
+	printf("%c + %c = %c\n", '1', '1', '1' + '1');
+	printf("%s\n", "This is a String");*/
+
+	/*printf("[안내]\n");
+	printf("인터넷 변경하세요\n");
+	printf("통신사 관계없이\n\\현금\\ 등 40만원+ \n할인 37%% !!\b\n\n");
+	printf("<최 / 대> \"2천 ~ 9천\"가능\n");
+	printf("통화버튼(☎) 눌러주세요~");*/
+}
