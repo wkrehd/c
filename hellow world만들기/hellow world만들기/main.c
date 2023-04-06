84275bebdb9af8c1cf424bd69543cef003677f20
vhswo <cba0898@gmail.com>
2023-04-06 15:53:42 +0900

    2023/4/6
    변수
    c언어 예약어 (int , char, float, double)
    자료형:(암시적,명시적)형변환
    scanf

--- hellow world만들기/hellow world만들기/main.c
+++ hellow world만들기/hellow world만들기/main.c
@@ -1,6 +1,59 @@
 #include<stdio.h>
+
 void main()
 {
+	int a = 0, b = 0;
+	scanf_s("%d %d", &a, &b);
+	printf_s("%d + %d = %d\n", a, b, a+b);
+
+	getchar();
+	/*int Num = 10;
+	float Fnum = 10.5;
+	double Dnum = 10.1;
+	char ch = 'a';
+
+	int result = Num + Fnum;
+	double d_result;
+	printf("%d + %.2f = %d\n", Num, Fnum , result);
+	result = Fnum + Dnum;
+	printf("%.2f + %.2f = %d\n", Fnum, Dnum, result);
+	d_result = ch + Num;
+	printf("%c + %d = %.2lf", ch, Num, d_result);*/
+	/*int num;
+	float Fnum = 1.5f;
+
+	num = Fnum;
+	printf("%d\n", num);
+	Fnum = num;
+	printf("%f\n", Fnum );
+
+	printf("%f\n", Fnum +num);
+	printf("%d\n", (int)Fnum+num);*/
+
+	/*int num = 5, ret;
+	char ch = 'A';
+
+	ret = num + 'A';
+	printf("ret = %d\n", ret);
+	printf("ret= %c\n", ret);*/
+
+	/*int age = 29;
+	float weight = 68.7, height = 169.52;
+	int num = 100;
+	char ch = 'A'; 
+
+	num = num + 1;
+	ch = 'B';
+	
+	printf("num= %d\n", num);
+	printf("ch= %c\n", ch);*/
+	/*printf("나의 나이는 %d세 입니다. \n", age);
+	printf("나의 몸무게는 %.1f(kg)입니다. \n", weight);
+	printf("나의 키는 %.2f(cm)입니다. \n", height);*/
+
+
+	/*int a, b = 8;
+
 	printf("Decimal : %d\n", 173);
 	printf("Decimal : %d\n", 0255);
 	printf("Decimal : %d\n\n", 0xAD);
@@ -11,7 +64,7 @@ void main()
 
 	printf("HexaDecimal : %x\n", 173);
 	printf("HexaDecimal : %x\n", 0255);
-	printf("HexaDecimal : %x\n", 0xad);
+	printf("HexaDecimal : %x\n", 0xad);*/
 
 	/*printf("%s %s\n", "Hello,",  "World");
 	printf("375 X 543 = %d\n", 375 * 543);
