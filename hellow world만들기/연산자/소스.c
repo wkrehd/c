2f42c9119e992ce0f50570784ea49ec2d7d1b838
reilap <wldns0384@naver.com>
2023-04-13 17:02:32 +0900

    2023/4/13
    비트연산자
    조건문 if ~else 퀴즈1 4번까지

--- hellow world만들기/연산자/소스.c
+++ hellow world만들기/연산자/소스.c
@@ -2,14 +2,45 @@
 void main()
 {
 	int num;
-	printf("1.정수를 입력하시오 : \n");
+	printf("1.정수를 입력하시오 : ");
 	scanf_s("%d", &num);
 	if (num % 3 == 0)
 	{
-		printf("%d는 3의배수 입니다.", num);
+		printf("%d는 3의배수 입니다.\n", num);
 	}
 	else
 	{
-		printf("%d는 3의배수가 아닙니다.", num);
+		printf("%d는 3의배수가 아닙니다.\n", num);
 	}
+
+	int num1;
+	printf("2.정수를 입력하시오 : ");
+	scanf_s("%d", &num1);
+	(num1 >= 0) ? printf("%d의 절대값은 %d입니다.\n", num1, num1)
+		: printf("%d의 절대값은 %d입니다\n",num1, -num1 );
+	
+	int num2;
+	printf("3.정수를 입력하시오 : ");
+	scanf_s("%d", &num2);
+	if (num2 % 2 == 0)
+	{
+		printf("%d는 짝수 입니다.\n", num2);
+	}
+	else
+	{
+		printf("%d는 홀수 입니다.\n", num2);
+	}
+
+	int num3, num4;
+	printf("정수 두개를 입력하시오 : ");
+	scanf_s("%d%d",&num3,&num4);
+	if (num3 >= num4)
+	{
+		printf("Max = %d\n", num3);
+	}
+	else
+	{
+		printf("Max = %d\n", num4);
+	}
+
 }
