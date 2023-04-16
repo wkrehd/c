37b846461c8ae6671a4322095ca9cb084caa2d4b
wkrehd <129727170+wkrehd@users.noreply.github.com>
2023-04-16 21:59:27 +0900

    2023/4/16
    if1문제 끝

--- hellow world만들기/연산자/소스.c
+++ hellow world만들기/연산자/소스.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-	/*int num;
+	int num;
 	printf("1.정수를 입력하시오 : ");
 	scanf_s("%d", &num);
 	if (num % 3 == 0)
@@ -41,6 +41,22 @@ void main()
 	else
 	{
 		printf("Max = %d\n", num4);
+	}
+	/*int num5, num6, num7;
+	printf("5. 정수 세개를 입력하시오 : ");
+	scanf_s("%d%d%d", &num5, &num6, &num7);
+	if ((num5 > num6) && (num5 > num7))
+	{
+		printf("Max = %d\n", num5);
+	}
+	else if ((num6 > num5) && (num6 > num7))
+	{
+		printf("Max = %d\n", num6);
+	}
+	else
+	{
+		printf("Max = %d\n", num7);
+		(ret <= num6) ? printf("Max = %d\n", num6) : printf("Max = %d\n", num7);
 	}*/
 	int num5, num6, num7;
 	printf("5. 정수 세개를 입력하시오 : ");
@@ -50,5 +66,43 @@ void main()
 	{
 		printf("Max = %d\n", num5);
 	}
-	(ret <= num6) ? printf("Max = %d\n", num6) : printf("Max = %d\n", num7);
+	else if (ret <= num6)
+	{
+		printf("Max = %d\n", num6);
+	}
+	else 
+	{
+		printf("Max = %d\n", num7);
+	}
+	int num8, num9;
+	printf("6.정수 두개를 입력하시오 : ");
+	scanf_s("%d%d", &num8, &num9);
+	if ((num8 >= num9) && (num8 % 2 == 0))
+	{
+		printf("큰수는 %d이며 짝수입니다\n", num8);
+	}
+	else if ((num8 >= num9) )
+	{
+		printf("큰수는 %d이며 홀수입니다.\n", num8);
+	}
+	else if ((num8 <= num9) && (num9 % 2 == 0))
+	{
+		printf("큰수는 %d이며 짝수입니다.\n", num9);
+	}
+	else
+	{
+		printf("큰수는 %d이며 홀수입니다.\n", num9);
+	}
+	int num10, num11;
+	printf("7. 정수 두개를 입력하시오 : ");
+	scanf_s("%d%d", &num10,&num11);
+	int sum = num10 + num11;
+	if ((sum % 2 != 0) && (sum %  3 == 0))
+	{
+		printf("%d 와 %d의합 : %d\n홀수이며 3의배수", num10, num11, sum);
+	}
+	else
+	{
+		printf("해당사항이 없습니다.\n");
+	}
 }
