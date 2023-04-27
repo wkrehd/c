eb83cb2ead1f039e39697413a92642561e1ac03f
reilap <wldns0384@naver.com>
2023-04-27 17:05:55 +0900

    2023/4/27
    함수 예제 적기 완료

--- hellow world만들기/함수/함수/함.cpp
+++ hellow world만들기/함수/함수/함.cpp
@@ -1,29 +1,62 @@
 #include<stdio.h>
-int Sum(int x, int y)
+#define PLAYER1 1//플레이어 뒤에 숫자를 붙인 이유?
+#define PLAYER2 2
+#define DRAW 3
+int Vs(int P1, int P2)
 {
-	int answer;
-	answer = x + y;
-	return answer;
+	if (P1 > P2)
+		return PLAYER1;
+	else if (P1 < P2)
+		return PLAYER2;
+	else
+		return DRAW;
 }
-void AccrueSum()
-{
-	int num1,  sum = 0;
-	printf("정수를 입력하시오 : ");
-	scanf_s("%d", &num1);
-	for (int i = 1; i <= num1; i++)
-		sum += i;
-	printf("1~%d의 누적합계 : %d\n", num1, sum);
-}
-void AccrueSum(int num1)
-{
-	int sum = 0;
-	for (int i = 1; 1 <= num1; i++)
-		sum += i;
-	printf("1~%d의 누적합계 : %d\n", num1,sum);
-}
+//int Sum(int x, int y)
+//{
+//	int answer;
+//	answer = x + y;
+//	return answer;
+//}
+//void AccrueSum()
+//{
+//	int num1,  sum = 0;
+//	printf("정수를 입력하시오 : ");
+//	scanf_s("%d", &num1);
+//	for (int i = 1; i <= num1; i++)
+//		sum += i;
+//	printf("1~%d의 누적합계 : %d\n", num1, sum);
+//}
+//void AccrueSum(int num1)
+//{
+//	int sum = 0;
+//	for (int i = 1; 1 <= num1; i++)
+//		sum += i;
+//	printf("1~%d의 누적합계 : %d\n", num1,sum);
+//}
+//int accruesum(int num1)
+//{
+//	int sum = 0;
+//	for (int i = 1; i <= num1; i++)
+//		sum += i;
+//	return sum;
+//}
+//float Avg(int num1, int num2) 
+//{
+//	float Avg;
+//	Avg = (float)(num1 + num2) / 2.0f;
+//	return Avg;
+//}
+//void show_avg(int num1, int num2, float avg)//출력x 아닌가요?
+//{
+//	printf("%d와 %d의 평균은 %0.2f입니다.", num1,num2,avg);
+//}
 
 void main()
 {
+	/*int num1;
+	printf("정수를 입력하시오 : ");
+	scanf_s("%d", &num1);
+	AccrueSum(num1);*/
 	/*int num1, num2;
 	printf("더할 두 정수를 입력하시오");
 	scanf_s("%d%d", &num1, &num2);
@@ -36,10 +69,40 @@ void main()
 	for (int i = 1; i <= num1; i++)
 		sum += i;
 	printf("1~%d의 누적합계 : %d\n", num1 ,sum);*/
-	AccrueSum();
-	int num1;
+	/*AccrueSum();*/
+	/*int num1;
 	printf("정수를 입력하시오 : ");
 	scanf_s("%d", &num1);
-	AccrueSum(num1);
+	printf("1~%d의 누적합계 : %d\n", num1 ,accruesum(num1));*/
 
+	/*int num1, num2;
+	float avg;
+	printf("두 정수를 입력하시오 : ");
+	scanf_s("%d%d", &num1,&num2);
+	avg = Avg(num1, num2);
+	show_avg(num1, num2, avg);*/
+
+	int Player1, Player2;
+	printf("Player1의 전투력 : ");
+	scanf_s("%d", &Player1);
+	printf("Player2의 전투력 : ");
+	scanf_s("%d", &Player2);
+	switch (Vs(Player1, Player2))
+	{
+	case PLAYER1:
+	{
+		printf("Player1 승리!!");
+		break;
+	}
+	case PLAYER2:
+	{
+	printf("Player2 승리!!");
+	break;
+	}
+	case DRAW:
+	{
+		printf("~무승부~");
+		break;
+	}
+	}
 }
