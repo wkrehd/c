92ad4883bfbfdaa1e097b4570c542cc3d039b66e
wkrehd <129727170+wkrehd@users.noreply.github.com>
2023-05-15 10:19:07 +0900

    2023/5/15
    다이아 문제 못품

--- hellow world만들기/함수/함수/함수 exe예.cpp
+++ hellow world만들기/함수/함수/함수 exe예.cpp
@@ -2,18 +2,33 @@
 //Height, width 는  Height*2+1이다
 void Diamake(int num)
 {
-	int num ;
+	/*int num = 0;*/
 	int height = num * 2 + 1;
 	int width = num * 2 + 1;
 	
-	for (int num =0; num <= height; num++)//height의 크기를 설정하는것
+	for (int i =1; i <= height; i++)//height의 크기를 설정하는것
 	{
-		for (int num =0; num<= width; num++)//width의 크기를 설정하는것
+		for (int j = 1; num + j <= width; j++)//width의 크기를 설정하는것
 		{
-			if (num !=num+1)//별이 처음엔 1개 다음엔2개씩 마지막엔 1개
-				printf("  ");//
-			printf("★");
+			int x = 0;//x의 좌표
+			if(x =num+j)
+				printf("★");	
+			else
+				printf("  ");
 		}
+		/*{
+			if (num + i > 0)
+				printf("★");
+			else
+				printf("  ");
+		}
+		for (int i = 1; num - 1 >= 1; i--)
+		{
+			if (num - 1 > 0)
+				printf("★");
+			else
+				printf("  ");
+		}*/
 		printf("\n");
 	}
 	
@@ -21,7 +36,7 @@ void Diamake(int num)
 }
 void main()
 {
-	int num ;
+	int num =0;
 	printf("세로 입력 : ");
 	scanf_s("%d", &num);
 	Diamake(num);
