60e0ef686dd31bf6caea73f4f8f0a86f64a23f81
vhswo <>
2023-05-25 15:54:05 +0900

    2023/5/25
    f9(중단점, 브레이크포인트)를 터지기전에 걸고
    f10을 이용해서 하나씩 확인
    f11로 함수를 들어가서 확인할수있음
    함수를 만드는 이유
    1.재사용을 위해서 (만들때부터 생각해야함)
    2.재사용이 안된다면 가독성을 위해서 만들어야함

--- hellow world만들기/UpDown 게임/UpDown 게임/UpDown.cpp
+++ hellow world만들기/UpDown 게임/UpDown 게임/UpDown.cpp
@@ -2,15 +2,111 @@
 #include<stdlib.h>
 #include<Windows.h>
 #include<time.h>
-#define UpDown_Num 1
-int Min = 1;
-int Max = 100;
+int min = 1;
+int max = 100;
+int turn = 10;
+
+// 최소(min)에서 최대(max) 범위의 값을 랜덤하게 구한다.
+int RandRange(int min, int max)
+{
+	return rand() % (max - min + 1) + min;
+}
+
+void ScopeSetting()
+{
+	int select, min1, max1,turn1;
+	while(1)
+	{ 
+	system("cls");
+	printf("=======게임 설정=======\n");
+	printf(" 1.최대,최소범위 설정\n");//함수로
+	printf(" 2.최대 Turn수 설정\n");//함수로
+	printf(" 3.돌아가기\n");
+	printf("입력 : ");
+	scanf("%d", &select);
+	system("cls");
+		switch (select)
+		{
+		case 1:
+
+			min1 = min;
+			printf("<현재 : %d>최소 값 변경 : ", min);
+			scanf("%d", &min);
+			max1 = max;
+			printf("<현재 : %d>최대 값 변경 : ", max);//최대값이 최소값보다 커야함
+			scanf("%d", &max);
+			if (max <= min)
+			{
+				printf("오류 : 최소값이 최대값 보다 큽니다 다시 입력하세요.\n");
+				system("pause");
+				min = min1;
+				max = max1;
+				system("cls");//case1을 다시 실행해야함
+			}
+
+			break;
+		case 2:
+			turn1 = turn;
+			printf("<현재 : %d>최대 Turn 값 변경 : ", turn);
+			scanf("%d", &turn);
+			if (turn<=0)
+			{
+				printf("오류 : 최대 턴수가 0보다 작거나 같습니다 다시입력하세요.\n");
+				system("pause");
+				turn = turn1;
+				system("cls");
+			}
+			break;
+		case 3:
+			return;
+		}
+	}
+}
+
+void GameStart()
+{
+	int num, rnum;
+	int updown = RandRange(min, max);
+	//rnum = rand() % (max - min + 1) + min;//함수로 바꾸기
+	//updown = rnum;
+		for (int i = 0; i < turn; i++)
+		{
+			system("cls");
+			printf("범위 : %d~%d\n", min, max);
+			printf("제한 Turn : %d\n", turn);
+			printf("========%d턴=========\n", i + 1);
+			printf("입력 : ");
+			scanf("%d", &num);
+			if (updown == num)
+			{
+				system("cls");
+				printf("정답!!  턴수 : %d\n", i);
+				system("pause");
+				break;
+
+			}
+			else if (updown > num)
+			{
+				printf("up!!\n");
+				system("pause");
+				system("cls");
+				continue;
+
+			}
+			else if (updown < num)
+			{
+				printf("Down!!\n");
+				system("pause");
+				system("cls");
+				continue;
+
+			}
+		}		
+}
 
 void Title()
 {
-	int RNum;
-	int select,num;
-	int UpDown[UpDown_Num] = { 0 };
+	int select;
 	while (1)
 	{
 		system("cls");
@@ -23,47 +119,14 @@ void Title()
 		system("cls");
 		switch (select)
 		{
-		case 1://자주 쓰이기 때문에 함수로
-			RNum = rand() % (Max - Min + 1) + Min;//1~100범위
-			UpDown[UpDown_Num] = RNum;
-			while (1)
-			{
-				for (int i = 0; i < 10; i++)
-				{
-					system("cls");
-					printf("범위 : %d~%d\n", Min, Max);
-					printf("제한 Turn : 10\n");
-					printf("========%d턴=========\n", i + 1);
-					printf("입력 : ");
-					scanf("%d", &num);
-					if (UpDown[UpDown_Num] == num)
-					{
-						system("cls");
-						printf("정답!!  턴수 : %d\n", i);
-						system("pause");
-						break;
-						
-					}
-					else if(UpDown[UpDown_Num] > num)
-					{ 
-						printf("up!!\n");
-						system("pause");
-						system("cls");
-						continue;
-						
-					}
-					else if (UpDown[UpDown_Num] < num)
-					{
-						printf("Down!!\n");
-						system("pause");
-						system("cls");
-						continue;
-						
-					}
-						
-				}
-				break;
-			}
+		case 1:
+			GameStart();
+			break;
+		case 2:
+			ScopeSetting();
+			break;
+		case 3:
+			return;
 		}
 
 	}
