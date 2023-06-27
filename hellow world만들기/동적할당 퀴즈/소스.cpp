7c0336b26d6a91299519d32dfc6e88277f6287eb
wkrehd <129727170+wkrehd@users.noreply.github.com>
2023-06-27 10:48:38 +0900

    2023/6/27
    studentcount+1을 하면 안되는 이유?

--- hellow world만들기/동적할당 퀴즈/소스.cpp
+++ hellow world만들기/동적할당 퀴즈/소스.cpp
@@ -13,7 +13,13 @@ typedef struct student
 	int number;
 }Student;
 
-
+void Swap(Student *st1, Student* st2)
+{
+	Student st3;
+	st3 = *st1;
+	*st1 = *st2;
+	*st2 = st3;
+}
 
 //학생의 번호, 나이, 성별, 학년을 보여주는 함수
 void ShowStudent(Student* st)//매개변수 student_List[i]을 받았는대 *st로 받을수 있는 이유는 *st의 시작주소와 student_List[i]의 시작주소가 같기 때문이다 배열의 이름은 시작주소를 가지므로
@@ -92,6 +98,16 @@ int SetStudent(Student* st, int studentcount)//st포인터에 값을 입력
 //등록된 학생들을 학년별로 나누는 함수 //등록된 학생들을 정렬을 하고 출력하도록 만들기 
 void GradeStudent(Student* studentList[], int studentcount)//배열로 받았으나
 {
+	for (int i = 0; i < studentcount; i++)
+	{
+		for (int j = i + 1; j < studentcount  ; j++)//studentcount+1 을 하면 안되는 이유는?
+		{
+			if (studentList[i]->grade > studentList[j]->grade)//오름차순
+			{
+				Swap(studentList[i], studentList[j]);
+			}
+		}
+	}
 	//printf("┏━━━━━━━━1 학년━━━━━━━━┓\n");
 	//for (int i = 0; i < studentcount; i++)
 	//{
@@ -121,18 +137,30 @@ void GradeStudent(Student* studentList[], int studentcount)//배열로 받았으나
 		switch (studentList[i]->grade)//swhitch (num) num자리의 숫자로 바로가게됨 없으면 switch를 빠져나옴 break가 없으면 접근한곳부터 break가 있는 코드까지 실행이됨
 		{
 		case 1:
-			printf("┏━━━━━━━━1 학년━━━━━━━━┓\n");
-			ShowStudent(studentList[i]);
+			printf("┏━━━━━━━━ 1 학년 ━━━━━━━━┓\n");
+			for (int i = 0; i < studentcount; i++)
+				{
+				if (1 == studentList[i]->grade)
+						ShowStudent(studentList[i]);
+				}
 			printf("┗━━━━━━━━━━━━━━━━━━━┛\n");
 			break;
 		case 2 :
-			printf("┏━━━━━━━━2 학년━━━━━━━━┓\n");
-			ShowStudent(studentList[i]);
+			printf("┏━━━━━━━━ 2 학년 ━━━━━━━━┓\n");
+			for (int i = 0; i < studentcount; i++)
+			{
+				if (2 == studentList[i]->grade)
+				ShowStudent(studentList[i]);
+			}
 			printf("┗━━━━━━━━━━━━━━━━━━━┛\n");
 			break;
-		default :
-			printf("┏━━━━━━━━3 학년━━━━━━━━┓\n");
-			ShowStudent(studentList[i]);
+		case 3 :
+			printf("┏━━━━━━━━ 3 학년 ━━━━━━━━┓\n");
+			for (int i = 0; i < studentcount; i++)
+			{
+				if (3 == studentList[i]->grade)
+				ShowStudent(studentList[i]);
+			}
 			printf("┗━━━━━━━━━━━━━━━━━━━┛\n");
 			break;
 		}
@@ -191,7 +219,7 @@ void main()
 			break;
 		case 3:
 			GradeStudent(student_List, studentcount);//배열에 있는 값이 필요하기 때문에 배열을 매개변수로 사용함
-			//왜 배열이 필요하나면 변수를 *student_List[MAX]형태 즉 변수를 배열로 만들었기때문에 
+			//왜 배열이 필요하나면 변수를 *student_List[MAX]형태  변수를 배열로 만들었기때문에 
 			break;
 		case 4:
 			break;
