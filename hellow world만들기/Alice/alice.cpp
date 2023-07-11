3e10885ae3ce38f015713a74704581a5d0242da5
vhswo <>
2023-07-11 16:00:44 +0900

    2023/7/11 strcmp, data structure(자료구조).재귀함수
    strcmp는  LHS (left hand side) 변수가  RHS (right hand side) 변수보다
    아스키코드값이 크면은 1을 반환한다
    재귀함수 예제 쓰기

--- hellow world만들기/Alice/alice.cpp
+++ hellow world만들기/Alice/alice.cpp
@@ -4,9 +4,8 @@
 
 void main()
 {
-	char oneline[256];//읽기 배열
-	char* findline[256];//글자 찾는 배열
-	*findline = (char*)malloc(sizeof(char));
+	char oneline[256] = {0};//읽기 배열
+	char findline[256] = {0};//글자 찾는 배열
 	FILE *fp;//변수 지정 Alice.txt파일이 있는 주소를 저장하기 위해서
 	fp = fopen("Alice.txt", "r");//만들어진 Alice.txt를 읽기 상태로 연다 
 	if (fp == NULL)
@@ -18,13 +17,25 @@ void main()
 			fgets(oneline, sizeof(oneline), fp);//한줄씩 정보를 가져오기위해 fgets (가리키는 포인터, 문자열길이,파일의 포인터)
 			printf("%s", oneline);
 		}
-		char word[];
-		scanf("%s" word[]);
-		printf("= Check Word Input : %s", &);
-		strcpy(*findline, oneline);//값을 복사해야되서*findline함
 
-		free(*findline);
-		*findline = NULL;
+		char checkword[256]={0};
+		int count = 0;
+		printf("Check Word Input : %s", checkword);
+		scanf("%s", checkword);
+
+		fp = fopen("Alice.txt", "r");
+		while (!feof(fp))//찾을려는 문자열을 찾기위해서 다시 한줄씩 정보를 가져옴
+		{
+			fgets(oneline, sizeof(oneline), fp);
+			//strcpy(findline, oneline);//oneline의 한줄값을 *findline에 복사함
+			//strcmp(oneline, checkword);//*findline과 checkword을 비교해서 같은것이 있으면 num++을 함
+			if (1 == strcmp(oneline, checkword))
+			{
+				count++;
+			}
+		}
 		fclose(fp);
+
+		printf("KMP Check to Count : %d", count);
 	}
 }
